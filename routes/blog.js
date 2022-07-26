56c2bce704cc25ea9389368ae64f4ec628d4ad5a
davidheweb <davidhermosilladuro@gmail.com>
2022-07-26 11:35:04 +0200

    Updating and deleting posts (documents)

--- routes/blog.js
+++ routes/blog.js
@@ -48,6 +48,20 @@ router.get("/posts/:id", async function (req, res) {
   res.render("post-detail", { post: post });
 });
 
+router.get("/posts/:id/edit", async function (req, res) {
+  const postId = req.params.id;
+  const post = await db
+    .getDb()
+    .collection("posts")
+    .findOne({ _id: new ObjectId(postId) }, { title: 1, summary: 1, body: 1 });
+
+  if (!post) {
+    return res.status(404).render("404");
+  }
+
+  res.render("update-post", { post: post });
+});
+
 router.post("/posts", async function (req, res) {
   const authorId = new ObjectId(req.body.author);
   const author = await db
@@ -69,6 +83,35 @@ router.post("/posts", async function (req, res) {
 
   const result = await db.getDb().collection("posts").insertOne(newPost);
   console.log(result);
+  res.redirect("/posts");
+});
+
+router.post("/posts/:id/edit", async function (req, res) {
+  const postId = req.params.id;
+  const result = await db
+    .getDb()
+    .collection("posts")
+    .updateOne(
+      { _id: new ObjectId(postId) },
+      {
+        $set: {
+          title: req.body.title,
+          summary: req.body.summary,
+          body: req.body.content,
+        },
+      }
+    );
+
+  res.redirect("/posts");
+});
+
+router.post("/posts/:id/delete", async function (req, res) {
+  const postId = req.params.id;
+  const result = await db
+    .getDb()
+    .collection("posts")
+    .deleteOne({ _id: new ObjectId(postId) });
+
   res.redirect("/posts");
 });
 
